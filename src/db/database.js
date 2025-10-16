76281a3ac58917fafd9329ef77bbab389023e6ea
payelll <ps9255401@gmail.com>
2025-10-17 03:06:48 +0530

    utils updated

---
+++ src/db/database.js
@@ -0,0 +1,17 @@
+console.log("🔍 MONGODB_URI from .env:", process.env.MONGODB_URI);
+
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
---
+++ src/utils/ApiError.js
@@ -0,0 +1,16 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message= "Something went Wrong",
+        errors = [],
+        statck = ""
+
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false,
+        this.errors = errors
+    }
+}
